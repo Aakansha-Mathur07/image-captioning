79f2550da4fc62b44fb1c93cd5b8a128cee06420
Aakansha Mathur <36303226+Aakansha-Mathur07@users.noreply.github.com>
2020-11-23 22:09:57 +0400

    preprocess-ready

--- model-1.py
+++ model-1.py
@@ -1,9 +1,14 @@
-from preprocess import get_features
+from preprocess import get_data
 
 def main():
-    features =  get_features(r'C:/Users/aakan/Desktop/CSCI 1470/Final Project- Image Captioning/dataset/Images')
+    #This returns a dictionary. The image_captions is a dictionary {image_name, captions}
+    #The features is a dictionary {image_name, pixels}
+    #The image shape is 32,32,3
+    directory = r'C:/Users/aakan/Desktop/CSCI 1470/Final Project- Image Captioning/dataset/Images'
+    file_path = r'C:/Users/aakan/Desktop/CSCI 1470/Final Project- Image Captioning/dataset/captions.txt'
+    image_captions, features =  get_data(directory, file_path)
     print(len(features))
-    print(features)
+    print(len(image_captions))
     
 
 if __name__ == '__main__':
--- preprocess.py
+++ preprocess.py
@@ -5,9 +5,19 @@ from keras.preprocessing.image import img_to_array
 from keras.models import Model
 import tensorflow as tf
 import os
+import nltk 
+nltk.download('stopwords')
+nltk.download('punkt')
+import string 
+import re 
+from nltk.corpus import stopwords 
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem.porter import PorterStemmer 
+import collections
 
-def get_features(directory):
+def get_data(directory, file_path):
     features = {}
+    image_captions = collections.defaultdict(list)
     for name in listdir(directory):
         filename = directory + '/' + name
         image = load_img(filename, target_size=(32, 32))
@@ -16,5 +26,18 @@ def get_features(directory):
         input_arr = input_arr/255.0
         image_name = name.split('.')[0]
         features[image_name] = input_arr
-        
-    return features
+
+    with open(file_path, 'r') as f:
+        train_data = f.read()
+    for line in train_data.split('\n'):
+        line_data = line.split(',')
+        image_name, captions =  line_data[0], line_data[1]
+        captions = captions.lower()
+        tokenizer = RegexpTokenizer(r'\w+')
+        captions = tokenizer.tokenize(captions)
+        stop_words = set(stopwords.words("english")) 
+        captions = [word for word in captions if word not in stop_words] 
+        captions = [PorterStemmer().stem(word) for word in captions]
+        image_name = image_name.split('.')[0]
+        image_captions[image_name].append(captions)
+    return image_captions, features
