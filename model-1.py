a41f134d4efada50fb0b2b0cd41ef386dcdecbd0
Aakansha Mathur <36303226+Aakansha-Mathur07@users.noreply.github.com>
2020-11-23 22:03:00 +0400

    some-updates

--- model-1.py
+++ model-1.py
@@ -3,6 +3,7 @@ from preprocess import get_features
 def main():
     features =  get_features(r'C:/Users/aakan/Desktop/CSCI 1470/Final Project- Image Captioning/dataset/Images')
     print(len(features))
+    print(features)
     
 
 if __name__ == '__main__':
