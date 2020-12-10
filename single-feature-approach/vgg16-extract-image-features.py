1066d0620e9a95abf938e23fe6148b728565fd98
Aakansha Mathur <36303226+Aakansha-Mathur07@users.noreply.github.com>
2020-12-11 00:26:55 +0400

    final-update

--- single-feature-approach/vgg16-extract-image-features.py
+++ single-feature-approach/vgg16-extract-image-features.py
@@ -19,7 +19,7 @@ def get_data(directory):
     model = Model(inputs=model.inputs, outputs=model.layers[-1].output)
     for name in listdir(directory):
         filename = directory + '/' + name
-        image = load_img(filename, target_size=(299, 299))
+        image = load_img(filename, target_size=(224, 224))
         image = img_to_array(image)
         image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
         image = preprocess_input(image)
